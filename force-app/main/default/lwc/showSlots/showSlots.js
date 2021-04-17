cf44fc23b2a8d8fa938e78373493911dc68fe46d
David Fromstein <david.fromstein@salesforce.com>
2021-04-16 22:37:42 -0700

    Made a few UI improvements

--- force-app/main/default/lwc/showSlots/showSlots.js
+++ force-app/main/default/lwc/showSlots/showSlots.js
@@ -1,27 +1,25 @@
-/*
-Copyright 2021 David Fromstein
-Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
-1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
-2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
-
 import { LightningElement, api, wire, track } from 'lwc';
 import { FlowAttributeChangeEvent } from 'lightning/flowSupport';
 //import { createRecord } from 'lightning/uiRecordApi';
 
 const ITEM_CLASS = 'slds-item';
+const DAY_SPACING_CLASS = 'slds-p-top_small';
 const SELECTED_CLASS = 'selected';
 const DOT = '.';
+const VALIDATION_MESSAGE = 'Please select a slot.'
 
 export default class ShowSlots extends LightningElement {
+
+    @api error;
 
     @api startTime;
     @api endTime;
 
     @api slots = [];
     @track selectedSlot;
-    showInGroupsOf = 10;
+
+    showInGroupsOf = 5;
+    numberToDisplay = this.showInGroupsOf;
     
     get selectedSlotIndex() {
         return this._selectedSlotIndex;
@@ -32,6 +30,14 @@ export default class ShowSlots extends LightningElement {
     }
     _selectedSlotIndex;
 
+    get displayedSlots() {
+        return this.slots.slice(0, this.numberToDisplay);
+    }
+
+    get showAddSlots() {
+        return this.displayedSlots.length < this.slots.length;
+    }
+
     @api
     validate() {
         if (this.selectedSlot) {
@@ -39,16 +45,22 @@ export default class ShowSlots extends LightningElement {
         } else {
             return { 
                 isValid: false ,
-                errorMessage: 'Please select a slot.'
+                errorMessage: VALIDATION_MESSAGE
             };
         }
     }
 
     connectedCallback() {
-        console.log('slots = ' + JSON.stringify(this.slots));
         let tempSlots = [];
         for (let i=0; i<this.slots.length; i++) {
-            tempSlots.push(Object.assign({index: i}, this.slots[i]));
+            let tempSlot = {
+                index: i,
+                firstInDay: i===0 || new Date(this.slots[i].start).getDate() !== new Date(this.slots[i-1].start).getDate(),
+                get spaceClass() {
+                     return this.firstInDay && i ? DAY_SPACING_CLASS : null
+                }
+            }
+            tempSlots.push(Object.assign(tempSlot, this.slots[i]));
         }
         this.slots = tempSlots;
     }
@@ -66,10 +78,11 @@ export default class ShowSlots extends LightningElement {
             else
                 item.classList.remove(SELECTED_CLASS);
         }
-        //this.dispatchEvent(new FlowAttributeChangeEvent('selectedSlot', this.selectedSlot));
         this.dispatchEvent(new FlowAttributeChangeEvent('startTime', this.selectedSlot.start));
         this.dispatchEvent(new FlowAttributeChangeEvent('endTime', this.selectedSlot.finish));
     }
 
-
+    showMoreSlots() {
+        this.numberToDisplay += this.showInGroupsOf;
+    }
 }
